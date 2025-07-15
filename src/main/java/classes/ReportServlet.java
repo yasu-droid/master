f935cef9aa57a763e1f2851c34546565d2903da8
yasu-droid <95845809+yasu-droid@users.noreply.github.com>
2025-07-15 18:29:06 +0900

    ReportServlet.java を更新 修正

--- src/main/java/classes/ReportServlet.java
+++ src/main/java/classes/ReportServlet.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpSession;
 public class ReportWeekServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public ReportServlet() {
+	public ReportWeekServlet() {
 		super();
 	}
 
